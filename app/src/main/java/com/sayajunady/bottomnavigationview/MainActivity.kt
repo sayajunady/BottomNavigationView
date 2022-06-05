05d73aae5980fab6d5e50b473d39f8b5e01ae988
Kopi Kode <junadhi@sar.ac.id>
2022-06-05 20:55:30 +0700

    Initial commit

---
+++ app/src/main/java/com/sayajunady/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sayajunady.bottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.favFragment, R.id.musicFragment, R.id.newsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
